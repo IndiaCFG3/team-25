ea14f2cff1219cc0853d7f23204200cbcc19c577
Saumya Verma <saumyadsta@gmail.com>
2020-08-16 16:55:40 +0530

    sms urls

---
+++ tillio/scripts/urls.py
@@ -0,0 +1,18 @@
+from django.conf.
+
+import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djtwilio.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    # Here we add our Twilio URLs
+    url(r'^sms/$', 'djtwilio.views.sms'),
+    url(r'^ring/$', 'djtwilio.views.ring'),
+)
