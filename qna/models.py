4d12e90ac6d3a960e4048c56cebbe0edb3745f65
Himanshu Shekhar <himanshu.shekharjha.272@gmail.com>
2020-08-16 11:17:12 +0530

    [ADD] Classroom model

--- qna/models.py
+++ qna/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class ClassRoom(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    class_id = models.CharField(max_length=10, null=False)
+
+    def __str__(self):
+        return self.name
+
+
